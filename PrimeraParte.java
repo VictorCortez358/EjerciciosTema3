44372f740bc1b71f0d307e85fcb85eaffba7d0c7
VictorCortez358 <00022120@uca.edu.sv>
2022-09-08 16:19:41 -0600

    Ejercicios tema 3

---
+++ PrimeraParte.java
@@ -0,0 +1,11 @@
+public class PrimeraParte {
+    public static void main(String[] args) {
+        var resultado = suma(3,5,6);
+
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a,int b,int c){
+        return a+b+c;
+    }
+}
